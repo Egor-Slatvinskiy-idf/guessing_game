07d5bd2cd2a8f4f19ca805d63417fd725ebae77d
Egor-Slatvinskiy-idf <egor.slatvinskiy@idfinance.com>
2022-08-21 16:37:38 +0300

    fix repositories

---
+++ packages/presentation/lib/bloc/bloc_state.dart
@@ -0,0 +1,37 @@
+import 'package:equatable/equatable.dart';
+
+abstract class GuessedState extends Equatable {
+  final int counter;
+
+  const GuessedState({required this.counter});
+
+  @override
+  List<Object?> get props => [counter];
+}
+
+class GuessedInitialState extends GuessedState {
+  final String rNumber;
+
+  const GuessedInitialState({
+    required super.counter,
+    required this.rNumber,
+  });
+}
+
+class GuessedSuccessState extends GuessedState {
+  final bool isGuessed;
+
+  const GuessedSuccessState({
+    required this.isGuessed,
+    required super.counter,
+  });
+}
+
+class GuessedFailureState extends GuessedState {
+  final bool isGuessed;
+
+  const GuessedFailureState({
+    required this.isGuessed,
+    required super.counter,
+  });
+}
